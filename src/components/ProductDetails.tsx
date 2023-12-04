3f27a060924d2a152d7b0e2de866d898de9c9db7
Gowthami karnati <karnatigowthami43@gmail.com>
2023-12-04 11:46:19 +0530

    responsive

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -75,6 +75,125 @@
 // };
 
 // export default ProductDetails;
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import Navbar from './Navbar';
+// import Products from './Products';
+// import { Product } from './Product';
+// import ProductSizes from './ProductSizes';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faShoppingCart, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+// import { useDispatch } from 'react-redux';
+// import { addToCart } from '../features/cart/actions';
+
+// interface ProductDetailsProps {
+//   id: string;
+// }
+
+// const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
+//   const dispatch = useDispatch();
+//   const [productDetails, setProductDetails] = useState<Product | null>(null);
+//   const [loading, setLoading] = useState<boolean>(false);
+//   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+//   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+//   useEffect(() => {
+//     const selectedProduct = Products.find((product) => product.id === Number(id));
+
+//     if (selectedProduct) {
+//       setProductDetails(selectedProduct);
+//     }
+//   }, [id]);
+
+//   const handleAddToCart = async () => {
+//     if (productDetails) {
+//       try {
+//         setLoading(true);
+//         await new Promise((resolve) => setTimeout(resolve, 1000));
+//         dispatch(addToCart(productDetails));
+//         setSuccessMessage('Item added to Cart');
+//       } catch (error) {
+//         setErrorMessage('Error adding item to Cart');
+//       } finally {
+//         setLoading(false);
+//       }
+//     }
+//   };
+
+//   const handleBuyNow = () => {
+//     console.log('Buy Now:', productDetails);
+//     // Assuming a synchronous operation for simplicity
+//     setSuccessMessage('Item purchased. It will be delivered in 2 days.');
+//   };
+
+//   if (!productDetails) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <div style={{ display: 'flex' }}>
+//         <div style={{ flex: 1, marginLeft: '100px', marginTop: '20px' }}>
+//           <img
+//             src={productDetails.image}
+//             alt={productDetails.name}
+//             style={{ width: '600px', height: '550px', objectFit: 'cover' }}
+//           />
+//           <div style={{ marginTop: '10px', width: '600px' }}>
+//             <button
+//               style={{
+//                 width: '295px',
+//                 height: '50px',
+//                 marginRight: '10px',
+//                 backgroundColor: 'orange',
+//                 border: 'none',
+//                 fontWeight: 'bold',
+//                 color: 'white',
+//                 cursor: 'pointer',
+//               }}
+//               onClick={handleAddToCart}
+//             >
+//               <FontAwesomeIcon icon={faShoppingCart} style={{ marginRight: '8px' }} />
+//               {loading ? 'Adding to Cart...' : 'Add to Cart'}
+              
+//             </button>
+//             <button
+//               style={{
+//                 width: '295px',
+//                 height: '50px',
+//                 backgroundColor: '#FF5733',
+//                 border: 'none',
+//                 fontWeight: 'bold',
+//                 color: 'white',
+//                 marginTop: '10px',
+//                 cursor: 'pointer',
+//               }}
+//               onClick={handleBuyNow}
+//             >
+//               <FontAwesomeIcon icon={faShoppingBag} style={{ marginRight: '8px' }} />
+//               Buy Now
+//             </button>
+//             {successMessage && (
+//               <p style={{ marginTop: '10px', color: 'green', fontWeight: 'bold' }}>{successMessage}</p>
+//             )}
+//             {errorMessage && (
+//               <p style={{ marginTop: '10px', color: 'red', fontWeight: 'bold' }}>{errorMessage}</p>
+//             )}
+//           </div>
+//         </div>
+//         <div style={{ flex: 1, padding: '16px' }}>
+//           <h1>{productDetails.name}</h1>
+//           <p>Brand: {productDetails.brand}</p>
+//           <p>Price: ₹{productDetails.price}</p>
+//           <ProductSizes sizes={productDetails.sizes} />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductDetails;
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
@@ -83,15 +202,16 @@ import { Product } from './Product';
 import ProductSizes from './ProductSizes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addToCart } from '../features/cart/actions';
-
+import { RootState } from '../store/store';
 interface ProductDetailsProps {
   id: string;
 }
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
   const dispatch = useDispatch();
+  const cartItems = useSelector((state: RootState) => state.cart.items);
   const [productDetails, setProductDetails] = useState<Product | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
@@ -107,11 +227,28 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
 
   const handleAddToCart = async () => {
     if (productDetails) {
+      const isInCart = cartItems.some((item) => item.id === productDetails.id);
+
+      if (isInCart) {
+        setErrorMessage('Item is already in the cart');
+
+        // Clear the error message after 5 seconds
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 1000);
+
+        return;
+      }
+
       try {
         setLoading(true);
         await new Promise((resolve) => setTimeout(resolve, 1000));
         dispatch(addToCart(productDetails));
+
         setSuccessMessage('Item added to Cart');
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 1500);
       } catch (error) {
         setErrorMessage('Error adding item to Cart');
       } finally {
@@ -124,6 +261,9 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
     console.log('Buy Now:', productDetails);
     // Assuming a synchronous operation for simplicity
     setSuccessMessage('Item purchased. It will be delivered in 2 days.');
+    setTimeout(() => {
+      setSuccessMessage(null);
+    }, 1500);
   };
 
   if (!productDetails) {
@@ -131,21 +271,21 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
   }
 
   return (
-    <div>
+    <div style={{ maxWidth: '1600px', margin: '0 auto' }}>
       <Navbar />
-      <div style={{ display: 'flex' }}>
-        <div style={{ flex: 1, marginLeft: '100px', marginTop: '20px' }}>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div style={{ flex: '0 0 100%', maxWidth: '600px', margin: '20px', marginLeft:'80px' }}>
           <img
             src={productDetails.image}
             alt={productDetails.name}
-            style={{ width: '600px', height: '550px', objectFit: 'cover' }}
+            style={{ width: '100%',  height: '500px' }}
           />
-          <div style={{ marginTop: '10px', width: '600px' }}>
+          <div style={{ marginTop: '10px' }}>
             <button
               style={{
-                width: '295px',
+                width: '100%',
                 height: '50px',
-                marginRight: '10px',
+                marginBottom: '10px',
                 backgroundColor: 'orange',
                 border: 'none',
                 fontWeight: 'bold',
@@ -156,17 +296,15 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
             >
               <FontAwesomeIcon icon={faShoppingCart} style={{ marginRight: '8px' }} />
               {loading ? 'Adding to Cart...' : 'Add to Cart'}
-              
             </button>
             <button
               style={{
-                width: '295px',
+                width: '100%',
                 height: '50px',
                 backgroundColor: '#FF5733',
                 border: 'none',
                 fontWeight: 'bold',
                 color: 'white',
-                marginTop: '10px',
                 cursor: 'pointer',
               }}
               onClick={handleBuyNow}
@@ -182,7 +320,7 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ id }) => {
             )}
           </div>
         </div>
-        <div style={{ flex: 1, padding: '16px' }}>
+        <div style={{ flex: '1', padding: '16px' }}>
           <h1>{productDetails.name}</h1>
           <p>Brand: {productDetails.brand}</p>
           <p>Price: ₹{productDetails.price}</p>
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,46 +1,78 @@
+// // // ProductList.tsx
+// // import React from 'react';
+// // import { Product } from './Product';
+// // import ProductCard from './ProductCard';
+
+// // interface ProductListProps {
+// //   products: Product[];
+// // }
+
+// // const ProductList:React.FC<ProductListProps>=({ products }) =>{
+// //   return (
+// //     <div className="ProductList">
+// //       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+// //         {products.map(product => (
+// //           <ProductCard key={product.id} product={product} />
+// //         ))}
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// // export default ProductList;
 // // ProductList.tsx
 // import React from 'react';
 // import { Product } from './Product';
 // import ProductCard from './ProductCard';
-
+// import Grid from '@mui/material/Grid';
+// import LeftPanel from './LeftPanel';
 // interface ProductListProps {
 //   products: Product[];
 // }
 
-// const ProductList:React.FC<ProductListProps>=({ products }) =>{
+// const ProductList: React.FC<ProductListProps> = ({ products }) => {
 //   return (
-//     <div className="ProductList">
-//       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-//         {products.map(product => (
-//           <ProductCard key={product.id} product={product} />
-//         ))}
-//       </div>
+//     <div className="ProductList" >
+//       <Grid container spacing={2} style={{marginTop:'0.2vw'}}>
+//         {/* Left Panel */}
+//         <Grid item xs={2} style={{backgroundColor:'#f5f5f5'}}>
+//           <LeftPanel />
+//         </Grid>
+
+        
+//         <Grid item xs={10} style={{backgroundColor:'#e6f7ff'}}>
+//           <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+//             {products.map(product => (
+//               <ProductCard key={product.id} product={product} />
+//             ))}
+//           </div>
+//         </Grid>
+//       </Grid>
 //     </div>
 //   );
 // }
 
 // export default ProductList;
-// ProductList.tsx
 import React from 'react';
 import { Product } from './Product';
 import ProductCard from './ProductCard';
 import Grid from '@mui/material/Grid';
 import LeftPanel from './LeftPanel';
+
 interface ProductListProps {
   products: Product[];
 }
 
 const ProductList: React.FC<ProductListProps> = ({ products }) => {
   return (
-    <div className="ProductList" >
-      <Grid container spacing={2} style={{marginTop:'0.2vw'}}>
+    <div className="ProductList">
+      <Grid container spacing={2} style={{ marginTop: '0.2vw' }}>
         {/* Left Panel */}
-        <Grid item xs={2} style={{backgroundColor:'#f5f5f5'}}>
+        <Grid item xs={12} md={2} style={{ backgroundColor: '#f5f5f5' }}>
           <LeftPanel />
         </Grid>
 
-        
-        <Grid item xs={10} style={{backgroundColor:'#e6f7ff'}}>
+        <Grid item xs={12} md={10} style={{ backgroundColor: '#e6f7ff' }}>
           <div style={{ display: 'flex', flexWrap: 'wrap' }}>
             {products.map(product => (
               <ProductCard key={product.id} product={product} />
@@ -50,6 +82,6 @@ const ProductList: React.FC<ProductListProps> = ({ products }) => {
       </Grid>
     </div>
   );
-}
+};
 
 export default ProductList;
